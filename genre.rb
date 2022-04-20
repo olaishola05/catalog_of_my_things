a3e174ebcecd6800eb6eb00401629273715e6996
AkshithaReddy1899 <akshithareddy1899@gmail.com>
2022-04-20 15:01:47 +0530

    Implement add_music method

--- genre.rb
+++ genre.rb
@@ -1,8 +1,14 @@
 class Genre
-  attr_accessor :name, :items
+  attr_accessor :name, :items, :id
 
-  def initialize(name)
+  def initialize(_id, name)
+    @id = Random.rand(1...1000)
     @name = name
     @items = []
+  end
+
+  def add_music(item)
+    @items << item
+    item.genre = self
   end
 end
