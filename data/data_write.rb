839b4cfb227ae3ac0df89ba892ddcd1b88e44d6d
Ola Ishola <olaishola@hotmail.co.uk>
2022-04-20 09:37:01 +0100

    Change path to write & read file to store folder

--- data/data_write.rb
+++ data/data_write.rb
@@ -1,5 +1,5 @@
 def add_books
-  file = File.open('./data/book.json', 'w')
+  file = File.open('./store/book.json', 'w')
   books = @books.map do |book|
     { title: book.title, cover: book.cover_state }
   end
@@ -7,7 +7,7 @@ def add_books
 end
 
 def add_labels
-  file = File.open('./data/label.json', 'w')
+  file = File.open('./store/label.json', 'w')
   labels = @labels.map do |label|
     { title: label.title, color: label.color }
   end
